035476402284768ee7478c7eecaf32d48d421f22
Ashrith Puppala <ashrithpuppala07@gmail.com>
2025-11-15 16:48:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors({
   origin: [
